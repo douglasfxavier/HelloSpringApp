c486eb053627ea4c7ceddde5094169643b34cf65
Douglas Francois Xavier Silva <20151370421@ad.ifpb.edu.br>
2019-03-25 17:31:12 -0300

    Deletando classe de teste, e refatorando a classe HelloSpringApplication para Configuracao

--- src/main/java/br/edu/ifpb/progdist/hellospring/Configuracao.java
+++ src/main/java/br/edu/ifpb/progdist/hellospring/Configuracao.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HellospringApplication {
+public class Configuracao {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellospringApplication.class, args);
+		SpringApplication.run(Configuracao.class, args);
 	}
 
 }
